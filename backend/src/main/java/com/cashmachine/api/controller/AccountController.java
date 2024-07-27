d158182773eea51d9b7d5858db7927565aca7bd6
Daniel Toledo <dan992965@gmail.com>
2024-07-27 12:50:52 -0300

    fix: ajusta controller

--- backend/src/main/java/com/cashmachine/api/controller/AccountController.java
+++ backend/src/main/java/com/cashmachine/api/controller/AccountController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/api/accounts")
 public class AccountController {
 
     private final UserService userService;
